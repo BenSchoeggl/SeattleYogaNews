8f66ecee77fd2f35fe36591bb64b31619cf05caa
Josh Malters <frostymalts@gmail.com>
2014-12-01 22:50:05 -0800

    added footer and prettier ng-repeat objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 // javascript for find a teacher tab of seattleyoganews.com
 
 "use strict";
+
+var tasksUrl = 'https://api.parse.com/1/classes/reviews';
+var formIsShown = false;
+
+angular.module('CustomerReviewApp', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = '8KggxA5XW1zh5OoudfgNJLMHiwMjWenbfWOnjMUq';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'CZ5YUhxPMa8ObW9XU6W6dn39QdOCg9PvruwJ4rLc';
+    })
+    .controller('ReviewController', function($scope, $http) {
+        $scope.refreshTasks = function() {
+            $http.get(tasksUrl)
+                .success(function (data) {
+                    $scope.studios = data.results;
+                });
+        };
+        $scope.refreshTasks();
+    });
